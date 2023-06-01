88373a7fccfd93f033453e84918b1b065f9f5c33
bruxo <diogo.stein@outlook.com.br>
2023-06-01 14:45:02 -0300

    Finalized v1.0

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 // DOM ELEMENTS
 const studentForm = document.getElementById('studentForm');
 const studentsContainer = document.querySelector('.students');
-const nameInput = studentForm['name'];
-const ageInput = studentForm['age'];
-const rollInput = studentForm['roll'];
 
-const students = [
-  {
-    name: 'Diogo',
-    age: 40,
-    roll: 36,
-  },
-  {
-    name: 'Carla',
-    age: 40,
-    roll: 36,
-  },
-];
+// The ||(or) é usado no caso de não ter nada no localStorage, aí então é criado uma array vazia
+const students = JSON.parse(localStorage.getItem('students')) || [];
 
-const addStudent = (name, age, roll) => {};
+const addStudent = (name, age, roll) => {
+  students.push({
+    name,
+    age,
+    roll,
+  });
+
+  localStorage.setItem('students', JSON.stringify(students));
+
+  return { name, age, roll };
+};
 
 const createStudentElement = ({ name, age, roll }) => {
   const studentDiv = document.createElement('div');
@@ -28,10 +25,34 @@ const createStudentElement = ({ name, age, roll }) => {
 
   studentName.innerText = 'Student name: ' + name;
   studentAge.innerText = 'Student age: ' + age;
-  studentRoll.innerText = 'Student roll: ' + roll;
+  studentRoll.innerText = 'Student rolll: ' + roll;
 
   studentDiv.append(studentName, studentAge, studentRoll);
   studentsContainer.appendChild(studentDiv);
+
+  studentsContainer.style.display = students.length === 0 ? 'none' : 'flex';
 };
 
+studentsContainer.style.display = students.length === 0 ? 'none' : 'flex';
+
+// Passando a array de objetos 'student' e para cada item da array chamando a função createStudentElement
 students.forEach(createStudentElement);
+
+studentForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nameInput = studentForm['name'];
+  const ageInput = studentForm['age'];
+  const rollInput = studentForm['roll'];
+
+  const newStudent = addStudent(
+    nameInput.value,
+    ageInput.value,
+    rollInput.value
+  );
+
+  createStudentElement(newStudent);
+
+  nameInput.value = '';
+  ageInput.value = '';
+  rollInput.value = '';
+});
